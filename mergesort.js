69e89646ff63cc5524ab7c3e6cda2ab71d96b458
oyupanqui <osmar30asis@gmail.com>
2023-11-26 15:43:56 -0500

    added solution to recursion

--- mergesort.js
+++ mergesort.js
@@ -1,14 +1,43 @@
 function mergeSort (array) {
+    if (array.length === 1) {
+        return array
+    }
+
     const chunk = Math.floor(array.length / 2)
 
     const left = array.slice(0, chunk)
-    const right = array.silce(chunk, array.length)
+    const right = array.slice(chunk, array.length)
     
-    // Sort left half
-    // Sort right half
-    // Merge halves
-    return array
+    return merge(mergeSort(left), mergeSort(right))
 }
-//console.log(mergeSort([6, 5, 3, 1, 8, 7, 2, 4]))
 
-let arr = [6, 5, 3, 1, 8, 7, 2, 4]
+function merge (left, right) {
+    const result = []
+
+    let leftIx = 0
+    let rightIx = 0
+
+    while (leftIx < left.length && rightIx < right.length) {
+        if (left[leftIx] < right[rightIx]) {
+            result.push(left[leftIx])
+            leftIx++
+        } else {
+            result.push(right[rightIx])
+            rightIx++
+        }
+    }
+
+    while (leftIx < left.length) {
+        result.push(left[leftIx])
+        leftIx++
+    }
+
+    while (rightIx < right.length) {
+        result.push(right[rightIx])
+        rightIx++
+    }
+
+    return result
+}
+
+console.log(mergeSort([6, 5, 3, 1, 8, 7, 2, 4]))
