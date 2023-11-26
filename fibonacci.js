641e7114d8f6ba16352c7d07ea8979b48c0ea86f
oyupanqui <osmar30asis@gmail.com>
2023-11-25 23:02:54 -0500

    added solution

--- fibonacci.js
+++ fibonacci.js
@@ -24,16 +24,15 @@ function fibs (num) {
 //console.log(fibs(10))
 
 function fibsRec (num) {
-    let array
     if (num === 1) {
-        array = [0]
+        return [0]
     }
     if (num === 2) {
-        array = [0, 1]
+        return [0, 1]
     }
     if (num > 2) {
-        
+        return [...fibsRec(num - 1), fibsRec(num - 1)[num - 2] + fibsRec(num - 1)[num - 3]];
     }
-    return array
+    
 }
-console.log(fibsRec(5))
+//console.log(fibsRec(10))
